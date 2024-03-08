84468a6491f4337d938e220772d5f0e8b051e593
Tobi-Daniel <oloyebmv@gmail.com>
2024-03-08 12:24:32 +0100

    Completed update user and logout user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,16 +26,16 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
 
-  if (userExists) { 
-    res.status(400)
+  if (userExists) {
+    res.status(400);
     throw new Error("User already exists!");
   }
 
-  const user = await User.create({username, email, password});
-  
+  const user = await User.create({ username, email, password });
+
   if (user) {
-    generateToken(res, user._id)
-    res.status(201)
+    generateToken(res, user._id);
+    res.status(201);
     res.json({
       _id: user._id,
       name: user.username,
@@ -48,4 +48,33 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { loginUser, registerUser };
+const updateUserprofile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.body._id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    if (req.body.password) user.password = req.body.password;
+    await user.save();
+    res.status(200).json({
+      _id: user._id,
+      name: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found!");
+  }
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    message: "Logged Out Successfully",
+  });
+});
+
+export { loginUser, registerUser, updateUserprofile, logoutUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  loginUser,
+  registerUser,
+  updateUserprofile,
+  logoutUser,
+} from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
+router.route("/update").put(updateUserprofile);
+router.route("/logout").get(logoutUser);
 
 export default router;
